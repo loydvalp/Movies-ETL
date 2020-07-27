f602d9ab3076ff83bdc7a109c80d798288257eef
Valencia Loyd <loyd_valencia@yahoo.com>
2020-07-26 20:26:55 -0500

    updating challenge

---
+++ Challenge.py
@@ -0,0 +1,9 @@
+import json
+import pandas as pd
+import numpy as np
+import re
+import psycopg2
+from sqlalchemy import create_engine
+from config import db_password
+import time
+import sqlalchemy
